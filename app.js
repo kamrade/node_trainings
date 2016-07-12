91cca248adda9d6d7ebe876a9b3d606180d8469b
Denis Michailov <kamrad.reg@gmail.com>
2016-07-12 22:36:37 +1000

    Function's Scope and modules

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 var flight = require('./flight');
 
-flight.setOrigin('LAX');
-flight.setDestination('DCA');
-flight.setNumber(462);
+var pdxlax = {
+	number: 847,
+	origin: 'PDX',
+	destination: 'LAX'
+};
 
-console.log( flight.getInfo() );
+var pl = flight(pdxlax);
+pl.triggerDepart();
 
-var anotherFlight = require('./flight');
+console.log( pl.getInformation() );
 
-console.log( anotherFlight.getInfo() );
+var ausdca = {
+	number: 382,
+	origin: 'AUS',
+	destination: 'DCA'
+};
+
+var ad = flight(ausdca);
+console.log( ad.getInformation() );
+
+console.log( pl.getInformation() );
